d34b09bbf0edf4d4842eff090cae25a101c54ce8
Vladimir Reymer <vladimireymer@gmail.com>
2024-11-22 12:58:24 +0700

    CRUD functions completed

--- app/cli.py
+++ app/cli.py
@@ -2,64 +2,108 @@ from app import CRUD
 
 
 class CLI:
+    """Command line interface for library"""
+
     def __init__(self, crud_object: CRUD) -> None:
         self.crud = crud_object
         self.commands_description = {
-            "add [title] [author] [year]": "add book to library with specific title, author and year",
-            "del [id]": "delete book from library by id",
+            "help": "show all available commands",
+            "show": "show all books in library",
+            "add [title] [author] [year]": "add book to library",
+            "remove [id]": "remove book from library by it's id",
             "find -a [author]": "find books by author",
             "find -t [title]": "find books by title",
             "find -y [year]": "find books by year",
-            "show": "show all books in library",
-            "status [id] [new_status]": "change book status"
+            "status [id]": "switch book status ('in stock' or 'taken')"
         }
 
-        # Show commands description on start
-        print("Available commands:\n")
+        self.show_commands()
+        print('\nEnter Ctrl + C to exit\n')
+
+    def show_commands(self) -> None:
+        """Show all available commands"""
+
+        print("\nAvailable commands:\n")
         for command, description in self.commands_description.items():
             print(f"{command}: {description}")
 
-        print('\nEnter Ctrl + C to exit\n')
-
     def commands_handler(self):
-        """Input command and call specific CRUD function
-        """
+        """Input command and call specific CRUD function"""
 
-        try:  # Handle keyboard interrupt
-
+        try:  # Handle keyboard interruption
             while True:
-
                 command = input("Enter command: ")
 
                 # Parse input
                 cmd: str = command.split()[0]
                 args: list[str] = command.split()[1:]
 
-                # Handle commands
+                # Handle commands and call CRUD functions
                 match cmd:
+
+                    case 'help':
+                        self.show_commands()
+                        print('\n')
+
+                    case 'show':
+                        books: list[dict] = self.crud.show()
+
+                        print("Books currently in library: ", len(books), '\n')
+                        for book in books:
+                            print(
+                                f"ID: {book['id']}, title: {book['title']}, author: {book['author']}, year: {book['author']}, status: {book['status']}")
+                        print('\n')
+
                     case 'add':
+                        # Check if arguments was entered and call crud function
                         if len(args) < 3:
-                            print("add method must contain following arguments: [title], [author], [year]")
+                            print("add command must contain 3 arguments: title, author, year")
                         else:
                             self.crud.add(title=args[0], author=args[1], year=args[2])
-                    case 'del':
-                        try:
+
+                    case 'remove':
+                        # Check if book's id was entered
+                        if (args) < 1:
+                            print("remove command must contain 1 argument: book's id")
+                        else:
+                            # Check if book's id is number and call crud function
                             if args[0].isdigit():
                                 self.crud.remove(id=int(args[0]))
-                        except IndexError:
-                            print("del method must contain [id] argument")
+                            else:
+                                print("book's id has to be a number: 1, 2, etc..")
+
                     case 'find':
-                        pass
-                    case 'show':
-                        books = self.crud.show()
-                        for book in books:
-                            print(book)
+                        # Check if arguments was entered
+                        if len(args) < 2:
+                            print("find command must contain 2 arguments: mode (-a, -t, -y) and data (author, year or title)")
+                        else:  # Find and get books by filter and call crud function
+                            if args[0] == '-a':
+                                books = self.crud.find(author=args[1])
+                            if args[0] == '-t':
+                                books = self.crud.find(title=args[1])
+                            if args[0] == '-y':
+                                books = self.crud.find(year=args[1])
+
+                            # Show finded books
+                            print(len(books), "books was founded:\n")
+                            for book in books:
+                                print(
+                                    f"ID: {book['id']}, title: {book['title']}, author: {book['author']}, year: {book['author']}, status: {book['status']}")
+                            print('\n')
+
                     case 'status':
-                        try:
+                        # Check if book's id was entered
+                        if len(args) < 1:
+                            print("remove command must contain 1 argument: book's id")
+                        else:
+                            # Check if book's id is number and call crud function
                             if args[0].isdigit():
-                                self.crud.status(id=int(args[0]), status=args[1])
-                        except IndexError:
-                            print("status method must contain [id] [new_status] arguments")
+                                self.crud.status(id=int(args[0]))
+                            else:
+                                print("book's id has to be a number: 1, 2, etc..")
+
+                    case _:
+                        print(f"'{cmd}' command not regognized. Check the list of available by enter: help")
 
         except KeyboardInterrupt:
             pass
--- app/crud.py
+++ app/crud.py
@@ -19,21 +19,42 @@ class CRUD:
             pass
         else:
             with open(self.save_file, 'w') as file:
+                # create file structure
                 json.dump({'books': []}, file)
 
-    def __get_lib(self, file):
-        """Get list of books in library and last book id"""
+    def __get_lib(self):
+        """Get library and last book id from save file"""
 
-        # Load data from save file
-        library: dict[str, list[dict]] = json.load(file)
+        with open(self.save_file, 'r') as file:
 
-        # Get last book id
-        if len(library['books']) == 0:
-            last_id: int = 0
-        else:
-            last_id: int = library['books'][-1]['id']
+            # Load data from save file
+            library: dict[str, list[dict]] = json.load(file)
 
-        return library, last_id
+            # Get last book id
+            if len(library['books']) == 0:
+                last_id: int = 0
+            else:
+                last_id: int = library['books'][-1]['id']
+
+            return library, last_id
+
+    def __save_lib(self, library: dict) -> None:
+        """Save library to save file"""
+
+        with open(self.save_file, 'w') as file:
+            json.dump(library, file, indent=4)
+
+    def show(self) -> list[dict]:
+        """Get dict of all books in library
+
+        Returns:
+            list[dict]: books data
+        """
+
+        # Get library from save file
+        library, _ = self.__get_lib()
+
+        return library['books']
 
     def add(self, title: str, author: str, year: str) -> None:
         """Add new book to library
@@ -44,48 +65,48 @@ class CRUD:
             year (str): book's year
         """
 
-        with open(self.save_file, 'r+') as file:
+        # Get library and last id from save file
+        library, last_id = self.__get_lib()
 
-            # Get library and last id from save file
-            library, last_id = self.__get_lib(file)
+        # Create book object
+        book = {
+            'id': last_id + 1,
+            'title': title,
+            'author': author,
+            'year': year,
+            'status': "in stock"
+        }
 
-            # Create book object
-            book = {
-                'id': last_id + 1,
-                'title': title,
-                'author': author,
-                'year': year,
-                'status': "obtain"
-            }
+        # Add new book to library
+        library['books'].append(book)
 
-            # Append new book to save file
-            library['books'].append(book)
-            file.seek(0)
-            json.dump(library, file, indent=4)
+        # Save changes to save file
+        self.__save_lib(library)
 
     def remove(self, id: int) -> None:
-        """Remove book from library by id
+        """Remove book from library by it's id
 
         Args:
-            id (int): book's id
+            id (int): book's id (index = id - 1)
         """
 
-        with open(self.save_file, 'r+') as file:
+        try:  # Handle index error
 
-            # Get library and last id from save file
-            library, last_id = self.__get_lib(file)
+            # Get library from save file
+            library, _ = self.__get_lib()
 
-            # Find book index
-            for index, book in enumerate(library['books']):
-                if book['id'] == id:
-                    break
+            # Delete book at given id
+            del library['books'][id-1]
 
-            # Remove book from library by index
-            del library['books'][index]
+            # Change id for books after deleted
+            for i in range(id-1, len(library['books'])):
+                library['books'][i]['id'] = i + 1
 
             # Save changes to save file
-            file.seek(0)
-            json.dump(library, file, indent=4)
+            self.__save_lib(library)
+
+        except IndexError:
+            print("Library have no book with id ", id)
 
     def find(self, title: str = None, author: str = None, year: str = None) -> list[dict]:
         """Get dict of books by title OR author OR year
@@ -99,42 +120,47 @@ class CRUD:
             list (dict): books data
         """
 
-        pass
+        # Get library from save file
+        library, _ = self.__get_lib()
 
-    def show(self) -> list[dict]:
-        """Get dict of all books in library
+        # Handle search by specific argument
+        books = list()
+        if title is not None:
+            for book in (library['books']):
+                if title == book['title']:
+                    books.append(book)
+        if author is not None:
+            for book in (library['books']):
+                if author == book['author']:
+                    books.append(book)
+        if year is not None:
+            for book in (library['books']):
+                if year == book['year']:
+                    books.append(book)
 
-        Returns:
-            list[dict]: books data
-        """
+        return books
 
-        with open(self.save_file, 'r') as file:
-
-            # Get library and last id from save file
-            library, last_id = self.__get_lib(file)
-
-            return library['books']
-
-    def status(self, id: int, status: str) -> None:
+    def status(self, id: int) -> None:
         """Change status of book by id
 
         Args:
             id (int): book's id
         """
 
-        with open(self.save_file, 'r') as file:
+        try:  # Handle index error
 
-            # Get library and last id from save file
-            library, last_id = self.__get_lib(file)
+            # Get library from save file
+            library, _ = self.__get_lib()
 
-            # Find book index
-            for index, book in enumerate(library['books']):
-                if book['id'] == id:
-                    break
-
-            # Remove book from library by index
-            library['books'][index]['status'] = status
+            # Switch status for book with given id
+            status = library['books'][id-1]['status']
+            if status == 'in stock':
+                library['books'][id-1]['status'] = 'taken'
+            else:
+                library['books'][id-1]['status'] = 'in stock'
 
             # Save changes to save file
-            file.seek(0)
-            json.dump(library, file, indent=4)
+            self.__save_lib(library)
+
+        except IndexError:
+            print("Library have no book with id ", id)
